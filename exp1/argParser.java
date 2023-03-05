6009a10ba4e3190e9bdbc05f08ca1a14f89877d5
Zeng-WCh <lewis.zeng02@gmail.com>
2023-03-05 22:40:39 +0800

    Modified

--- exp1/argParser.java
+++ exp1/argParser.java
@@ -1,26 +1,99 @@
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
-public class argparser {
-    public enum argType {
-        argString,
-        argInt,
-        argDouble,
-        argBoolean
+enum argType {
+    argString,
+    argInt,
+    argDouble,
+    argBoolean
+}
+
+class args {
+    String trigger;
+    argType type;
+    String description;
+    String defaultVal;
+
+    public args() {
+        this.trigger = "";
+        this.description = "";
+        this.defaultVal = "";
     }
 
-    private String[] args;
-    private Map<String, String> mp;
+    public args(String trigger, argType type) {
+        this.type = type;
+        this.trigger = trigger;
+        switch (type) {
+            case argInt -> {
+                this.defaultVal = "0";
+                this.description = "An Integer instance";
+            }
+            case argBoolean -> {
+                this.defaultVal = "true";
+                this.description = "An Boolean instance";
+            }
+            case argDouble -> {
+                this.defaultVal = "0.0";
+                this.description = "An Double instance";
+            }
+            case argString -> {
+                this.defaultVal = "";
+                this.description = "An String instance";
+            }
+        }
+    }
+
+    public args(String trigger, argType type, String defaultVal) {
+        this.trigger = trigger;
+        this.type = type;
+        this.defaultVal = defaultVal;
+        switch (type) {
+            case argInt ->
+                this.description = "An Integer instance";
+            case argBoolean ->
+                this.description = "An Boolean instance";
+            case argDouble ->
+                this.description = "An Double instance";
+            case argString ->
+                this.description = "An String instance";
+        }
+    }
+
+    public args(String trigger, argType type, String defaultVal, String description) {
+        this.trigger = trigger;
+        this.type = type;
+        this.description = description;
+        this.defaultVal = defaultVal;
+    }
 
     /**
      *
-     * @param shortCommand, like "-i"
-     * @param longCommand, like "--interactive"
-     * @param type, an "argType" instance
-     * @param description, info that will be displayed when type --help
-     * @param defaultVal, if not given by the command line, then how it will be
+     * @return a formatted String instance, looks like:
+     * -i [argType], [defaultVal] [description]
      */
-    public void addArgs(String shortCommand, String longCommand, argType type, String description, String defaultVal){
+    public String helpInfo() {
+        String type = "";
+        switch (this.type) {
+            case argInt -> type = "int";
+            case argBoolean -> type = "boolean";
+            case argDouble -> type = "double";
+            case argString -> type = "string";
+        }
+        return String.format("%-5s %-8s, %-5s\t%-40s", this.trigger, type, this.defaultVal, this.description);
+    }
+}
+
+public class argParser {
+    private Map<String, String> shortMp;
+    private Map<String, String> longMp;
+
+    public argParser() {
+        this.shortMp = new HashMap<>();
+        this.longMp = new HashMap<>();
+    }
+
+    public void addArgs(String command, argType type, String description, String defaultVal) {
+
     }
 
 }
