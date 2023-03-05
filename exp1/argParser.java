06f7a72ea09f4c6cbdd808fa83694a6e5d1f277a
Zeng-WCh <lewis.zeng02@gmail.com>
2023-03-05 23:51:44 +0800

    Modefied

--- exp1/argParser.java
+++ exp1/argParser.java
@@ -2,98 +2,122 @@ import java.util.HashMap;
 import java.util.Map;
 
 enum argType {
-    argString,
-    argInt,
-    argDouble,
-    argBoolean
+    typeInt,
+    typeString,
+    typeBoolean,
+    typeDouble,
 }
 
-class args {
-    String trigger;
+class argException extends Exception {
+    public argException(String info) {
+        super(info);
+    }
+}
+
+
+/**
+ * Current Support Mode: --[trigger] [value], like --debug true
+ * TODO: -[shortTrigger] [value], like -d true
+ */
+class Args {
     argType type;
-    String description;
-    String defaultVal;
+    String trigger, description, defaultVal;
 
-    public args() {
-        this.trigger = "";
-        this.description = "";
-        this.defaultVal = "";
-    }
-
-    public args(String trigger, argType type) {
+    public Args(String trigger, argType type) {
         this.type = type;
         this.trigger = trigger;
-        switch (type) {
-            case argInt -> {
+        switch (this.type) {
+            case typeInt -> {
                 this.defaultVal = "0";
-                this.description = "An Integer instance";
+                this.description = "An Interger Instance";
             }
-            case argBoolean -> {
+            case typeBoolean -> {
                 this.defaultVal = "true";
-                this.description = "An Boolean instance";
+                this.description = "A Boolean Instance";
             }
-            case argDouble -> {
+            case typeDouble -> {
                 this.defaultVal = "0.0";
-                this.description = "An Double instance";
+                this.description = "A Double Instance";
             }
-            case argString -> {
+            case typeString -> {
                 this.defaultVal = "";
-                this.description = "An String instance";
+                this.description = "A String Instance";
             }
         }
     }
 
-    public args(String trigger, argType type, String defaultVal) {
+    public Args(String trigger, argType type, String defaultVal) {
+        this.defaultVal = defaultVal;
         this.trigger = trigger;
         this.type = type;
-        this.defaultVal = defaultVal;
-        switch (type) {
-            case argInt ->
-                this.description = "An Integer instance";
-            case argBoolean ->
-                this.description = "An Boolean instance";
-            case argDouble ->
-                this.description = "An Double instance";
-            case argString ->
-                this.description = "An String instance";
+        switch (this.type) {
+            case typeInt ->
+                this.description = "An Interger Instance";
+            case typeBoolean ->
+                this.description = "A Boolean Instance";
+            case typeDouble ->
+                this.description = "A Double Instance";
+            case typeString ->
+                this.description = "A String Instance";
         }
     }
 
-    public args(String trigger, argType type, String defaultVal, String description) {
-        this.trigger = trigger;
-        this.type = type;
-        this.description = description;
+    public Args(String trigger, argType type, String defaultVal, String description) {
         this.defaultVal = defaultVal;
+        this.description = description;
+        this.type = type;
+        this.trigger = trigger;
     }
 
     /**
-     *
-     * @return a formatted String instance, looks like:
-     * -i [argType], [defaultVal] [description]
+     * @return a formatted string, looks like "--[trigger]: [type], default=[defaultVal], [description]
      */
     public String helpInfo() {
-        String type = "";
+        String tp = "";
         switch (this.type) {
-            case argInt -> type = "int";
-            case argBoolean -> type = "boolean";
-            case argDouble -> type = "double";
-            case argString -> type = "string";
+            case typeInt ->
+                tp = "int";
+            case typeBoolean ->
+                tp = "boolean";
+            case typeDouble ->
+                tp = "double";
+            case typeString ->
+                tp = "string";
         }
-        return String.format("%-5s %-8s, %-5s\t%-40s", this.trigger, type, this.defaultVal, this.description);
+        return String.format("--%-10s: %-8s, default=%-20s, %s", this.trigger, tp, this.defaultVal, this.defaultVal);
     }
 }
 
 public class argParser {
-    private Map<String, String> shortMp;
-    private Map<String, String> longMp;
+    public static String getTrigger(String arg) {
+        String[] info = arg.split("--");
+        if (info.length == 0) {
+            return "";
+        }
+        else {
+            return info[0];
+        }
+    }
 
+    private Map<String, Args> pattern, mp;
     public argParser() {
-        this.shortMp = new HashMap<>();
-        this.longMp = new HashMap<>();
+        this.pattern = new HashMap<>();
+        this.mp = new HashMap<>();
     }
 
-    public void addArgs(String command, argType type, String description, String defaultVal) {
-
+    public void addArgs(String trigger, argType type) {
+        this.pattern.put(trigger, new Args(trigger, type));
     }
 
+    public void addArgs(String trigger, argType type, String defaultVal) {
+        this.pattern.put(trigger, new Args(trigger, type, defaultVal));
+    }
+
+    public void addArgs(String trigger, argType type, String defaultVal, String description) {
+        this.pattern.put(trigger, new Args(trigger, type, defaultVal, description));
+    }
+
+    public void parseArgs(String[] args) throws Exception {
+
+    }
 }
