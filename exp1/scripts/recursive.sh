b4c0cb3eed909ae3b91fa8c9eb63f7f3619e752b
Zeng-WCh <lewis.zeng02@gmail.com>
2023-03-23 15:36:23 +0800

    Finish Scripts

--- exp1/scripts/recursive.sh
+++ exp1/scripts/recursive.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TESTDIR=../test/recursive
+RESULTDIR=../result/recursive
+LOG=${RESULTDIR}/test_$(date +%s).log
 
 [ -d ${TESTDIR} ]
 if [ $? -ne 0 ];
@@ -9,9 +11,15 @@ then
     ./generate.py
 fi
 
+[ -d ${RESULTDIR} ]
+if [ $? -ne 0 ];
+then
+  mkdir -p ${RESULTDIR}
+fi
+
 # Run the test
 for i in $(ls ${TESTDIR}/*.in)
 do
     echo "Running test: "${i}
-    ./run.sh < ${i} > ${i%.in}.out
+    { time ./run.sh < ${i} > ${i%.in}.out; } 2>> ${LOG}
 done
