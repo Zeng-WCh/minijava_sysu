c9d942876131544f1d1521d540989bf3f0a10d40
Zeng-WCh <lewis.zeng02@gmail.com>
2023-05-06 00:34:12 +0800

    Removed Files

--- exp2/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd src
-javac -d ..\bin -classpath ..\bin token\*.java
-javac -d ..\bin -classpath ..\bin scanner\*.java
-javac -d ..\bin -classpath ..\bin ast\*.java
-javac -d ..\bin -classpath ..\bin parser\*.java
-cd ..
-pause
-@echo on
