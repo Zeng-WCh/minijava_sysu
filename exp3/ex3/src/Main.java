c4afd6386bba045e6b81deb3b314046f7996e519
Zeng-WCh <lewis.zeng02@gmail.com>
2023-06-01 16:38:53 +0800

    Modified

--- exp3/ex3/src/Main.java
+++ exp3/ex3/src/Main.java
@@ -12,70 +12,73 @@ public class Main {
 			for (int i = 0; i < argv.length; i++) {
 				int id = 0;
 				ComplexSymbolFactory symbolFactory = new ComplexSymbolFactory();
-				ScannerBuffer scanner = new ScannerBuffer(
-						new OberonScanner(new BufferedReader(new FileReader(argv[i])), symbolFactory));
+				// ScannerBuffer scanner = new ScannerBuffer(
+						// new OberonScanner(new BufferedReader(new FileReader(argv[i])), symbolFactory));
+				OberonScanner scanner = new OberonScanner(new BufferedReader(new FileReader(argv[i])), symbolFactory);
 				Parser p = new Parser(scanner, symbolFactory);
 				System.out.println(argv[i] + ":");
 				try {
 					p.parse();
 				} catch (Exception ex) {
-					// System.out.println(obj.getLine()+" line "+ obj.getCol()+" column");
+					// System.out.printf("<%d, %d>\n", scanner.getLine(), scanner.getColumn());
 					System.out.println(ex.getMessage());
+					System.exit(1);
 				}
-				moduleBlock mb = p.getAST();
-				declarations d = mb.declarations;
-				ArrayList<procedureDec> pd = d.procDecs;
-				for (procedureDec pdc : pd) {
-					boolean hasPar = false;
-					System.out.print(pdc.head.name + "(");
-					for (fp param : pdc.head.fp.fps) {
-						hasPar = true;
-						boolean isVar = param.isVar;
-						if (isVar) {
-							System.out.print("var ");
-						}
-						System.out.print(param.name + ":" + param.type.toString() + ",");
-					}
-					System.out.println((hasPar ? "\b" : "") + ")");
+				System.out.println("SUCCESS");
+			// 	moduleBlock mb = p.getAST();
+			// 	declarations d = mb.declarations;
+			// 	ArrayList<procedureDec> pd = d.procDecs;
+			// 	for (procedureDec pdc : pd) {
+			// 		boolean hasPar = false;
+			// 		System.out.print(pdc.head.name + "(");
+			// 		for (fp param : pdc.head.fp.fps) {
+			// 			hasPar = true;
+			// 			boolean isVar = param.isVar;
+			// 			if (isVar) {
+			// 				System.out.print("var ");
+			// 			}
+			// 			System.out.print(param.name + ":" + param.type.toString() + ",");
+			// 		}
+			// 		System.out.println((hasPar ? "\b" : "") + ")");
 
-					procedureBody pb = pdc.body;
-					stmts statements = pb.stmts;
-					ArrayList<stmt> stmtList = statements.statements;
-					for (stmt state:stmtList) {
-						// System.out.println(state);
-						ast a = state.getStmt();
-						if (a == null) {
-							continue;
-						}
-						if (a instanceof callStmt) {
-							callStmt c = (callStmt) a;
-							// String callee = c.name;
-							// actualParameters ap = c.params;
-							// System.out.printf("%s(", callee);
-							// boolean hasParam = false;
-							// for (expr ape:ap.exprs) {
-							// 	hasParam = true;
-							// 	System.out.printf("%s,", ape.toString());
-							// }
-							// System.out.println((hasParam ? "\b" : "") + ")");
-							System.out.println(c.toString());
-						}
-						else if (a instanceof ifStmt) {
-							ifStmt ifs = (ifStmt) a;
-							System.out.println(ifs.toString());
-						}
-						else if (a instanceof assignmentStmt) {
-							assignmentStmt asg = (assignmentStmt) a;
-							System.out.println(asg.toString());
-						}
-						else if (a instanceof whileStmt) {
-							whileStmt ws = (whileStmt) a;
-							System.out.println(ws.toString());
-						}
+			// 		procedureBody pb = pdc.body;
+			// 		stmts statements = pb.stmts;
+			// 		ArrayList<stmt> stmtList = statements.statements;
+			// 		for (stmt state:stmtList) {
+			// 			// System.out.println(state);
+			// 			ast a = state.getStmt();
+			// 			if (a == null) {
+			// 				continue;
+			// 			}
+			// 			if (a instanceof callStmt) {
+			// 				callStmt c = (callStmt) a;
+			// 				// String callee = c.name;
+			// 				// actualParameters ap = c.params;
+			// 				// System.out.printf("%s(", callee);
+			// 				// boolean hasParam = false;
+			// 				// for (expr ape:ap.exprs) {
+			// 				// 	hasParam = true;
+			// 				// 	System.out.printf("%s,", ape.toString());
+			// 				// }
+			// 				// System.out.println((hasParam ? "\b" : "") + ")");
+			// 				System.out.println(c.toString());
+			// 			}
+			// 			else if (a instanceof ifStmt) {
+			// 				ifStmt ifs = (ifStmt) a;
+			// 				System.out.println(ifs.toString());
+			// 			}
+			// 			else if (a instanceof assignmentStmt) {
+			// 				assignmentStmt asg = (assignmentStmt) a;
+			// 				System.out.println(asg.toString());
+			// 			}
+			// 			else if (a instanceof whileStmt) {
+			// 				whileStmt ws = (whileStmt) a;
+			// 				System.out.println(ws.toString());
+			// 			}
 
 
-					}
-				}
+					// }
+				// }
 			}
 		}
 	}
