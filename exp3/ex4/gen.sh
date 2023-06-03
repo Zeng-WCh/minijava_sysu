6f8c20cc3a2de87fe5cb2f8d0000464d58a53586
Zeng-WCh <lewis.zeng02@gmail.com>
2023-06-03 17:35:04 +0800

    Modified scripts

--- exp3/ex4/gen.sh
+++ exp3/ex4/gen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rm src/OberonScanner.java
+rm src/OberonScanner.java > /dev/null 2>&1
 java -jar ./jflex/jflex-1.9.1.jar -d src src/oberon.flex
-java -jar javacup/java-cup-11b.jar -interface -destdir src -parser Parser -symbols Symbol src/oberon.cup
